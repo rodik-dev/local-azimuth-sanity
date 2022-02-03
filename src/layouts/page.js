3df0ebbea1740015cbfd4f0c7f37b159c139c2f2
Stackbit Code Editor <projects@stackbit.com>
2022-02-04 00:19:59 +0200

    page.js: changed

--- src/layouts/page.js
+++ src/layouts/page.js
@@ -27,7 +27,7 @@ export default class Page extends React.Component {
                                 </div>
                             )}
                             {_.has(page, 'content') && (
-                                <div className="post-content">
+                                <div className="post-content" data-sb-field-path="content">
                                     {markdownify(_.get(page, 'content'))}
                                 </div>
                             )}
